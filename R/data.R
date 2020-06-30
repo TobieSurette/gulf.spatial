302e69ec4268d8023c556e40c45d2915bea4a936
ricardd <daniel.ricard@gmail.com>
2020-06-30 09:26:51 -0300

    documentation of Rda datasets

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Polygons of fishing zones boundaries
+#'
+#' A dataset containing the longitudes and latitudes of polygon vertices for
+#'  fishing zones for different species in the Gulf of St. Lawrence.
+#'  Also includes polygons with the coastline added.
+#'
+#' @format An R object of class simple feature data frame
+#'
+"fishing.zone.polygons"
