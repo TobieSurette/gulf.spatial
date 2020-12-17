b316cbb8f26c6a5610a515145ae7cae9c34ea5c5
TobieSurette <tobie.surette@gmail.com>
2020-12-17 11:42:01 -0400

    Added expanded zone 12 to kriging polygons

---
+++ build/kriging.polygons.R
@@ -0,0 +1,27 @@
+library(gulf.utils)
+library(gulf.spatial)
+
+b <- read.gulf.spatial("kriging polygons revised")
+
+zones <- c("zone12", "zoneE", "zoneF", "zone19_F_buffer", "zoneEF_unassigned", "zone19_12_buffer")
+
+# Join polygons together:
+p = st_sf(st_sfc(st_polygon(list(cbind(b[[zones[1]]]$longitude, b[[zones[1]]]$latitude)))))
+for (i in 2:length(zones)){
+   tmp = st_sf(st_sfc(st_polygon(list(cbind(b[[zones[i]]]$longitude, b[[zones[i]]]$latitude)))))
+   p <- st_union(p, tmp)
+}
+
+# Splice Maggies hole into polygon:
+p1 <- as.matrix(p)[[1]][[1]]
+p2 <- as.matrix(p)[[1]][[2]]
+
+ix <- which(p1[,1] == -61.04130)
+a <- rbind(p1[1:ix, ], p2, p1[ix:nrow(p1), ])
+
+plot(a[, 1], a[, 2], type = "l")
+
+b$zone12_expanded <- list(longitude = a[, 1], latitude = a[,2], name = "zone12_expanded")
+
+kriging.polygons <- b
+save(kriging.polygons, file = "build/kriging.polygons.revised.rda")
