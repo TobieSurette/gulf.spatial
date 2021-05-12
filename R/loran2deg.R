a296351314b17f7b98044b7d7d800698dadf8355
Tobie Surette <tobie.surette@gmail.com>
2021-05-12 14:06:59 -0300

    Added Loran XZ functions

--- R/loran2deg.R
+++ R/loran2deg.R
@@ -1,4 +1,4 @@
-#' Loran Coordinate Conversion
+#' @title Loran Coordinate Conversion
 #'
 #' @aliases loran
 #'
@@ -23,34 +23,17 @@
 #' # Convert a vector of Loran XY coordinates to decimal degree format:
 #' loran2deg(seq(14000, 14500, len = 21), seq(29000, 29500, len = 21))
 #'
-#' loran_xz_lat(14508.27,	43316.88) # Return latitude
-#' loran_xz_lon(14508.27,	43316.88) # Return longitude
-#' loranxz2deg(14508.27,	43316.88)
-#'
+#' # Loran XZ conversion:
+#' loranxz2deg(14508.27, z = 43316.88) # Return latitude
+
 #' @export
 loran2deg <- function(x, y, z){
-   x <- as.loran(x)
-   y <- as.loran(y)
+   if (!missing(x) & !missing(y)) return(loranxy2deg(x, y))
+   if (!missing(x) & !missing(z)) return(loranxz2deg(x, z))
+   return(NULL)
+}
 
-   index <- !is.na(x) & !is.na(y)
-   xsave <- x
-   ysave <- y
-   x <- x[index]
-   y <- y[index]
-
-   if (length(x) != length(y)) stop("'x' and 'y' must be the same length.")
-   if (length(x) == 0) stop("One or both of coordinate vectors are empty.")
-
-   if (length(x) > 1){
-      long <- rep(NA, length(x))
-      lat  <- rep(NA, length(x))
-      for (i in 1:length(x)){
-         r <- loran2deg(x[i], y[i])
-         long[i] <- r$long
-         lat[i] <- r$lat
-      }
-   }else{
-   #========================== LATITUDE CONVERSION =============================
+loranxy2lat <- function(x, y){
    Pi = 3.141592653
    p2 = Pi / 180;
    Ae = 6378206.4;
@@ -64,59 +47,29 @@ loran2deg <- function(x, y, z){
 
    dor1 = 2;
 
-   xd1 = 41;
-   xm1 = 15;
-   xs1 = 11.98;
-   xd2 = 69;
-   xm2 = 58;
-   xs2 = 40.51;
-   xd3 = 46;
-   xm3 = 48;
-   xs3 = 27.54;
-   xd4 = 67;
-   xm4 = 55;
-   xs4 = 39.35;
-   count1 = 4263.76;
-   delta1 = 11000;
-   yd1 = 46;
-   ym1 = 46;
-   ys1 = 32.62;
-   yd2 = 53;
-   ym2 = 10;
-   ys2 = 32.41;
-   yd3 = xd3;
-   ym3 = xm3;
-   ys3 = xs3;
-   yd4 = xd4;
-   ym4 = xm4;
-   ys4 = xs4;
+   xd1 = 41; xm1 = 15; xs1 = 11.98;
+   xd2 = 69; xm2 = 58; xs2 = 40.51;
+   xd3 = 46; xm3 = 48; xs3 = 27.54;
+   xd4 = 67; xm4 = 55; xs4 = 39.35;
+   count1 = 4263.76; delta1 = 11000;
+   yd1 = 46; ym1 = 46; ys1 = 32.62;
+   yd2 = 53; ym2 = 10; ys2 = 32.41;
+   yd3 = xd3;ym3 = xm3;ys3 = xs3;
+   yd4 = xd4;ym4 = xm4;ys4 = xs4;
    count2 = 7509.86;
    delta2 = 25000;
-   dd1 = 49;
-   dm1 = 51;
-   ds1 = 30.07;
-   dd2 = 64;
-   dm2 = 26;
-   ds2 = 41.18;
+   dd1 = 49; dm1 = 51; ds1 = 30.07;
+   dd2 = 64; dm2 = 26; ds2 = 41.18;
 
-   nstn = 1;
-   dur = 1;
-   xlIsect1 = 45;
-   xlIsect2 = 30;
-   xlIsect3 = 30;
-   xlIsect4 = 65;
-   xlIsect5 = 0;
-   xlIsect6 = 0;
+   nstn = 1; dur = 1;
+   xlIsect1 = 45; xlIsect2 = 30; xlIsect3 = 30; xlIsect4 = 65; xlIsect5 = 0; xlIsect6 = 0;
 
    pos11 = (xd1 + xm1 / 60 + xs1 / 3600) * p2;
    pos12 = (xd2 + xm2 / 60 + xs2 / 3600) * p2;
    pos21 = (xd3 + xm3 / 60 + xs3 / 3600) * p2;
    pos22 = (xd4 + xm4 / 60 + xs4 / 3600) * p2;
 
-   Php = pos11;
-   Flp = pos12;
-   Phs = pos21;
-   Fls = pos22;
+   Php = pos11; Flp = pos12; Phs = pos21; Fls = pos22;
 
    Dlon = Fls - Flp;
    Sl = sin(Dlon);
@@ -145,10 +98,7 @@ loran2deg <- function(x, y, z){
    pos41 = (yd3 + ym3 / 60 + ys3 / 3600) * p2;
    pos42 = (yd4 + ym4 / 60 + ys4 / 3600) * p2;
 
-   Php = pos31;
-   Flp = pos32;
-   Phs = pos41;
-   Fls = pos42;
+   Php = pos31; Flp = pos32; Phs = pos41; Fls = pos42;
 
    Dlon = Fls - Flp;
    Sl = sin(Dlon);
@@ -344,8 +294,9 @@ loran2deg <- function(x, y, z){
 
    Ang = pos51;
    lat = Ang * 180 / Pi;
-
-   #========================== LONGITUDE CONVERSION ============================
+   return(lat)
+}
+loranxy2lon <- function(x, y){
    Pi = 3.14159265358979;
    p2 = Pi / 180;
    Ae = 6378206.4;
@@ -636,32 +587,8 @@ loran2deg <- function(x, y, z){
    }
    Ang = pos52;
    long = -(Ang * 180 / Pi);
-
-   # Restore original vector.
-   xsave <- xsave * NA
-   ysave <- ysave * NA
-   xsave[index] <- long
-   ysave[index] <- lat
-   long <- xsave
-   lat <- ysave
-   }
-
-   return(data.frame(lat = lat, long = long))
+   return(long)
 }
-
-#' @export
-loranxz2deg <- function(x, z){
-
-   ix <- which(!is.na(x) & !is.na(z) & (x > 0) & (z > 0))
-
-   v <- rep(NA, length(x))
-   if (length(ix) > 0){
-      v[ix] <- data.frame(longitude = -loranxz2lon(x[ix], z[ix]), latitude = loranxz2lat(x[ix], z[ix]))
-   }
-
-   return(v)
-}
-
 loranxz2lat <- function(x, z){
    # Returns the latitude for a given loran X and Z coordinate:
    Pi = 3.14159265358979;   p2 = Pi / 180;
@@ -783,7 +710,6 @@ loranxz2lat <- function(x, z){
    Loranc1 = pos51 * 180 / Pi
    return(Loranc1)
 }
-
 loranxz2lon <- function(x, z){
    # Returns the longitude for a given loran X and Z coordinate:
     Pi = 3.14159265358979; p2 = Pi / 180; Ae = 6378206.4; Fl = 0.0033900753; rlax = 0.75;
@@ -1031,7 +957,58 @@ loranxz2lon <- function(x, z){
    return(Loranc2)
 }
 
-#' @describeIn loran2deg Clean up loran coordinate strings.
+#' @describeIn loran2deg Convert Loran XY coordinates.
+#' @export
+loranxy2deg <- function(x, y){
+   # Clean-up formatting:
+   x <- as.loran(x)
+   y <- as.loran(y)
+
+   # Target reasonable values:
+   ix <- which(!is.na(x) & !is.na(y) & (x > 0) & (y > 0))
+
+   # Perform conversion:
+   lon <- lat <- rep(NA, length(x))
+   if (length(ix) > 0){
+      for (i in 2:length(ix)){
+         lon[ix] <- -loranxy2lon(x[ix], y[ix])
+         lat[ix] <-  loranxy2lat(x[ix], y[ix])
+      }
+   }
+
+   # Results:
+   v <- data.frame(longitude = lon, latitude = lat)
+
+   return(v)
+}
+
+#' @describeIn loran2deg Convert Loran XZ coordinates.
+#' @export
+loranxz2deg <- function(x, z){
+   # Clean-up formatting:
+   x <- as.loran(x)
+   z <- as.loran(z)
+
+   # Target reasonable values:
+   ix <- which(!is.na(x) & !is.na(z) & (x > 0) & (z > 0))
+
+   # Perform conversion:
+   lon <- lat <- rep(NA, length(x))
+   if (length(ix) > 0){
+      for (i in 2:length(ix)){
+         lon[ix] <- -loranxz2lon(x[ix], z[ix])
+         lat[ix] <-  loranxz2lat(x[ix], z[ix])
+      }
+   }
+
+   # Results:
+   v <- data.frame(longitude = lon, latitude = lat)
+
+   return(v)
+}
+
+
+#' @describeIn loran2deg Clean up character strings containing Loran coordinates.
 #' @export
 as.loran <- function(x){
    # AS.LORAN - Reformats a character string to proper Loran-C numeric format.
@@ -1059,7 +1036,7 @@ as.loran <- function(x){
       r <- rep(NA, length(x))
 
       # Identify which strings have the proper format:
-      ii <- grep("^[123][0-9]{4,10}", x)
+      ii <- grep("^[1234][0-9]{4,10}", x)
       if (length(ii) > 0) r[ii] <- as.numeric(paste0(substr(x[ii], 1, 5), ".", substr(x[ii], 6, nchar(x[ii]))))
       full[index] <- r
    }
