83a8b9908d5676f9e24561618cae317870df3e8f
Tobie Surette <tobie.surette@gmail.com>
2025-09-15 15:02:15 -0300

    Added substrate info and function

---
+++ R/substrate.R
@@ -0,0 +1,46 @@
+#' @title Local Substrate Information
+#'
+#' @description Returns local sea-floor substrate information.
+#'
+#' @param longitude,latitude Numerical vector of longitudes in decimal degree format.
+#'
+#' @return Returns character vector of substrate composition (e.g. sand, gravel, etc.)
+#' the same length as the coordinate vectors.
+#'
+#' @references Loring and Nota, 1973, https://gcgeo.gc.ca/geonetwork/metadata/eng/8c269a91-d3a2-4f49-943d-6b2401c42cba, accessed April 10, 2025
+#'
+#' @examples
+#'    # Find the water depth at a single point:
+#'    substrate(63.8, 47.05)
+#'
+#'    # Find water depth for multiple points:
+#'    lat <- c(48, 47, 46.5)
+#'    long <- c(-64, -61.5, -62)
+#'    substrate(long, lat)
+#'
+#' @export substrate
+#'
+
+substrate <- function(longitude, latitude){
+   # To be fixed in a later version:
+   #file <- "C:/Users/SuretteTJ/Desktop/github/gulf.spatial/inst/extdata/shapefiles"
+
+   file <- locate.gulf.spatial(c("Seafloor", "shp"))
+   file <- paste0(strsplit(file, "shapefiles")[[1]][1], "shapefiles")
+
+   # Read seafloor substrate file:
+   subtrate <- sf::read_sf(dsn = file, layer = "Seafloor_SubstratBenthique")
+
+   # Create and re-project coordinate data:
+   p <- sf::st_as_sf(data.frame(longitude, latitude), crs = 4326, coords = c("longitude", "latitude"))
+   p <- sf::st_transform(p, crs = sf::st_crs(subtrate))
+
+   # Join points and substrate data:
+   p <- sf::st_join(p, subtrate, left = TRUE, sf::st_nearest_feature)[, c("DEPOT_EN")]
+   names(p) <- gsub("DEPOT_EN", "substrate", names(p))
+
+   # Clean-up:
+   p$substrate <- gsub("gravier", "gravel", tolower(p$substrate))
+
+   return(p$substrate)
+}
